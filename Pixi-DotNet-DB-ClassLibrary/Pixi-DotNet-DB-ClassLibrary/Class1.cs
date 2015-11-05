287978074cf679707fa771a6f822422f5bc20e4b
JockHA <jockha@outlook.dk>
2015-11-05 21:31:30 +0100

    Initial

---
+++ Pixi-DotNet-DB-ClassLibrary/Pixi-DotNet-DB-ClassLibrary/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Transactions;
+
+namespace Pixi.DB.ClassLibrary
+{
+    public class Class1
+    {
+
+
+        void LocalProc()
+        {
+            using (var scope = new TransactionScope())
+            {
+                using (var conn1 = new SqlConnection("  "))
+                {
+                    conn1.Open();
+
+                    SqlCommand cmd1 = conn1.CreateCommand();
+                    cmd1.CommandText = String.Format("Insert into tbale values(1)");
+                    cmd1.ExecuteNonQuery();
+
+
+
+                }
+                scope.Complete();
+            }
+
+        }
+    }
+}
